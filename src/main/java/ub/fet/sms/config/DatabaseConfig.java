0324015533659d855aa8ed185125fc1a1852b686
lobeserge <lobeserge100@gmail.com>
2020-07-03 20:26:20 +0100

    update

--- src/main/java/ub/fet/sms/config/DatabaseConfig.java
+++ src/main/java/ub/fet/sms/config/DatabaseConfig.java
@@ -1,5 +1,12 @@
 package ub.fet.sms.config;
 
+import com.zaxxer.hikari.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import javax.sql.DataSource;
+
+@Configuration
 public class DatabaseConfig {
 
+    
 }
